01300f9d52c534a75ee053ee6a864e2234b16e89
Jeffrey Luo <luojeffrey@yahoo.com>
2017-05-28 10:52:22 -0400

    Reconfigured test files

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import processing.core.*;
+
+public class Main extends PApplet {
+	public void setup(){
+		background(100,100,100);
+	}
+	
+	public void settings(){
+		size(300,300);
+		
+	}
+	
+	public void draw(){
+		
+	}
+}
