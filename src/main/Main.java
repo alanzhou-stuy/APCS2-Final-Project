063f07ce5c61ff703c237f3c3c529b36e2c5b920
Jeffrey Luo <luojeffrey@yahoo.com>
2017-05-28 11:06:59 -0400

    PApplet working

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,21 @@
 package main;
 
-import processing.core.*;
+import processing.core.PApplet;
 
 public class Main extends PApplet {
+	public static void main(String[] args){
+		PApplet.main("main.Main");
+	}
+	
 	public void setup(){
 		background(100,100,100);
 	}
 	
 	public void settings(){
-		size(300,300);
-		
+		size(700,700);
 	}
 	
 	public void draw(){
 		
-	}
+	}	
 }
