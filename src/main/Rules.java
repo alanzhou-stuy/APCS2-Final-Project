4ca3204d38bfe87c421315be4bd952ed25eef30c
Alan <azhou2@stuy.edu>
2017-06-01 08:38:14 -0400

    added a few functions

--- src/main/Rules.java
+++ src/main/Rules.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.awt.*;
+import java.awt.event.*;
+
 public class Rules {
 	private Tile current;
 	int timer;
 	private Colorizer colorizer;
 	Grid g;
+	
 
 	public Rules() {
 		timer = 0;
@@ -24,9 +28,9 @@ public class Rules {
 			} else {
 				current = colorizer.drop(current, 1);
 				current = colorizer.moveRight(current);
-				/*for (Square s : current.getSquares()) {
-					System.out.println(s.getYCor());
-				}*/
+				for (Square s : current.getSquares()) {
+					System.out.println(s.getXCor());
+				}
 			}
 		}
 		timer++;
@@ -43,7 +47,7 @@ public class Rules {
 	}
 
 	public boolean hitBlock() {
-		/*int[] white = new int[] { 255, 255, 255 };
+/*		int[] white = new int[] { 255, 255, 255 };
 		for (Square s: current.getSquares()) {
 			if (g.getColor((s.getYCor() + 35)) != white) {
 				return true;
@@ -54,7 +58,7 @@ public class Rules {
 
 	public boolean blockOffMap() {
 		for (Square s: current.getSquares()) {
-			if ((s.getXCor() > 300) || s.getYCor() > 715) { //need to change
+			if ((s.getXCor() < 325) || s.getYCor() > 715 || s.getXCor() > 640) { //need to change
 				return true;
 			}
 		}
