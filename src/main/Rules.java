df5dea4b93323365b4c3977ab32760fb57c3b564
Alan Zhou <azhou2@stuy.edu>
2017-06-04 21:22:10 -0400

    almost done with hitting blocks

--- src/main/Rules.java
+++ src/main/Rules.java
@@ -36,12 +36,15 @@ public class Rules {
 		int run_period = (int) ((-0.0235 * Math.pow(SPEED, 3) + 0.69 * Math.pow(SPEED, 2) - 7.85 * SPEED + 35) * (FRAMERATE / 60.0)) + 1;
 
 		if (TIMER % run_period == 0) {
-			if (hitBottom()
-			// || hitBlock()
-			) {
+			if (hitBottom()) {
 				current = colorizer.spawnBlock();
 			} else {
-				current = colorizer.drop(current, 1);
+				if (hitBlock()) {
+					current = colorizer.spawnBlock();
+				} 
+				else {
+					current = colorizer.drop(current, 1);
+				}
 
 				// current = colorizer.rotate(false, current, 1);
 
@@ -74,22 +77,13 @@ public class Rules {
 	}
 
 	public boolean hitBlock() {
-		int[] white = new int[] { 255, 255, 255 };
-		for (Square[] rowOfSquares : g.grid) {
-			for (Square s : rowOfSquares) {
-				for (Square s1 : current.getSquares()) {
-					// System.out.println(s1.getYCor() + g.squareSize);
-					// System.out.println(s.getYCor());
-					if (((s1.getYCor() + g.squareSize + 1) == s.getYCor()) && (s.getColor() != white)) {
-						return true;
-					}
+		for (Square s: current.getSquares()) {
+			//if (s.getYCor() == calLowestYCorOfTile(current)) {
+			if (colorizer.colored(s.getRowIndex(),s.getColIndex())) {
+					return true;
 				}
-			}
+			//}
 		}
-		/*
-		 * for (Square s: current.getSquares()) { if (g.getSquare(s.getXCor(),
-		 * (s.getYCor() + g.squareSize)).getColor() != white) { return true; } }
-		 */
 		return false;
 	}
 
@@ -111,6 +105,17 @@ public class Rules {
 		for (Square s : t.getSquares()) {
 			if (s.getYCor() < lowest) {
 				lowest = s.getYCor();
+			}
+		}
+		return lowest;
+	}
+	
+	public int calLowestYCorOfTileX(Tile t) {
+		int lowest = t.getSquares().get(0).getYCor();
+		for (Square s : t.getSquares()) {
+			if (s.getYCor() < lowest) {
+				lowest = s.getXCor();
+				return lowest;
 			}
 		}
 		return lowest;
@@ -161,6 +166,19 @@ public class Rules {
 			}
 		}
 	}
+	
+	/*public boolean hit() {
+		int[] white = new int[] { 255, 255, 255 };
+		int lowX = current.getSquares().get(0).getColIndex();
+		int lowY = current.getSquares().get(0).getRowIndex();
+		int highX = current.getSquares().get(0).getColIndex();
+		int highY = current.getSquares().get(0).getRowIndex();
+		for (Square s: current.getSquares()) {
+			if (s.getColIndex() < lowX) {
+			}
+		}
+		return false;
+	}*/
 
 	public class KeyAction extends AbstractAction {
 		private String sequence;
